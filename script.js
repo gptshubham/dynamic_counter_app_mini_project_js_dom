e46f180f455abbf649e0e3f17b0f5d3143c2e9f7
gptshubham <shubhamca76@gmail.com>
2024-12-22 23:59:55 +0530

    floating point number incrementation bug fixed

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 // Selecting dom elements
-const value = document.querySelector('#count')
-const increment = document.querySelector('#increment')
-const decrement = document.querySelector('#decrement')
-const user_input = document.querySelector('input')
-const reset = document.querySelector('#reset')
+const value = document.querySelector('#count');
+const increment = document.querySelector('#increment');
+const decrement = document.querySelector('#decrement');
+const user_input = document.querySelector('input');
+const reset = document.querySelector('#reset');
 
 // adding event listeners
 
 // for incrementing the count
-increment.addEventListener('click', () => value.innerHTML = Number(value.innerHTML) + Number(user_input.value))
+increment.addEventListener(
+  'click',
+  () =>
+    (value.innerHTML = parseInt(value.innerHTML) + parseInt(user_input.value))
+);
 
 // fot decrementing the count
-decrement.addEventListener('click', () => value.innerHTML -= user_input.value)
+decrement.addEventListener(
+  'click',
+  () => (value.innerHTML -= user_input.value)
+);
 
 // for reset
-reset.addEventListener('click', () => value.innerHTML = 0)
+reset.addEventListener('click', () => (value.innerHTML = 0));
